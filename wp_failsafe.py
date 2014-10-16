4ed670e748c649671a064fa0ba764a934399354c
Steve McGuire <stephen.mcguire@colorado.edu>
2014-10-15 19:37:23 -0600

    Added changes to reflect patched DroneAPI, system_status in the Vehicle class

--- wp_failsafe.py
+++ wp_failsafe.py
@@ -26,26 +26,32 @@ shouldQuit = False
 #This function ends up executing on the message recv thread from within DroneAPI - not the dedicated thread setup to run
 # function itself
 #The fix is patching DroneAPI to pass through system_status from the HEARTBEAT message
+# And the callback is still executed from the message recv thread. The right answer might include
+# a condition variable and a message queue
 
-def mavrx_debug_handler(message):
-    """A demo of receiving raw mavlink messages"""
-    #print "Received", message
-    typ = message.get_type()
-    if typ == 'HEARTBEAT':
-        if (message.system_status == 5):
-            print "Detected loss of GCS link, exiting"
-            shouldQuit = True
+#The point is, if you're executing something of any substance from this callback, beware of taking too much time, 
+#since messages won't be received while this function is executing
+
+def mode_callback(name):
+    global shouldQuit #since this is on the main MAVProxy thread, not the independent thread we've got
+    print "System status: %d" % ( v.system_status)
+    if (v.system_status == 5):
+        print "Detected loss of GCS link!"
+        shouldQuit = True
 
 # First get an instance of the API endpoint
 api = local_connect()
 # get our vehicle - when running with mavproxy it only knows about one vehicle (for now)
 v = api.get_vehicles()[0]
-v.set_mavlink_callback(mavrx_debug_handler)
-print "WP_Failsafe started, watching for GCS lost link"
 
-while not (threading.current_thread().exit or shouldQuit):   #until we are signalled to exit, everything is run from the callback
+print "WP_Failsafe started, watching for GCS lost link"
+v.add_attribute_observer("mode", mode_callback)
+
+while not (threading.current_thread().exit or shouldQuit):   
+    #until we are signalled to exit, 
+    #everything is run from the callback
     time.sleep(1)
-    print "ShouldQuit: %d" % (shouldQuit)
-v.set_mavlink_callback(None)
+
+v.remove_attribute_observer("mode", mode_callback)
 print "Unregistered callback, exiting"
 
