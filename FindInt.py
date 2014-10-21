779ec4e107678a46be7e15c94bf967cb934f7960
Paul Guerrie <pagu3177@colorado.edu>
2014-10-21 13:58:38 -0600

    Trying to add interpolation to increase reference point accuracy

--- FindInt.py
+++ FindInt.py
@@ -15,8 +15,9 @@ def FI(L1,Pathx,Pathy,ACStatex,ACStatey,ACStatez,ACStatechi):
 	temp = []
         #For every point on the path...
         for i in range(len(Pathx)):
-            temp.append(math.sqrt(math.pow((L1x[th]-Pathx[i]),2) + math.pow((L1y[th]-Pathy[i]),2)))#Calcualte the distance between each point on the 													    path and the L1[th] point on the circle.
+            temp.append(math.sqrt(math.pow((L1x[th]-Pathx[i]),2) + math.pow((L1y[th]-Pathy[i]),2)))#Calcualte the distance between each point on the path and the L1[th] point on the circle.
         Dist.append(min(temp))#the minimum of these distances is the smallest distance between that particular point on the L1 circle and the path.
+    L1IntInd = numpy.argmin(Dist)
     #plt.figure(1)
     #plt.plot(L1x,L1y,'r',Pathx,Pathy)
     #plt.show()
@@ -30,7 +31,7 @@ def FI(L1,Pathx,Pathy,ACStatex,ACStatey,ACStatez,ACStatechi):
 	if Dist[i] < Dist[i-1]:
 		if Dist[i] < Dist[i+1]:
 			if Dist[i] < TOLERANCE:
-				IntInds.append(i)#If an element is smaller than it's neighbors, then it is a local minimum, and if it is          							  within a certain tolerance, then it is an intersection.
+				IntInds.append(i)#If an element is smaller than it's neighbors, then it is a local minimum, and if it is within a certain tolerance, then it is an intersection.
     #For all the L1 circle intersection indicies, find the corresponding path indicies
     IntIndMaxPath = []
     for i in range(len(IntInds)):
@@ -41,7 +42,19 @@ def FI(L1,Pathx,Pathy,ACStatex,ACStatey,ACStatez,ACStatechi):
     #Find the most index positive intersection -> index of L1 circle
     IntIndMax = max(IntInds)
 
+    if IntIndMax == len(Pathx):
+	IndF = 1
+	IndR = IntIndMax - 1
+    else if IntIndMax == 1:
+	IndF = 1 + IntIndMax
+	IndR = len(Pathx)
+    else:
+	IndF = IntIndMax + 1
+	IndR = IntIndMax - 1
+
+    DR = math.sqrt(math.pow((L1x[L1IntInd]-Pathx[IndR]),2) + math.pow((L1y[L1IntInd]-Pathy[IndR]),2))
+
     INTx = Pathx[max(IntIndMaxPath)]
     INTy = Pathy[max(IntIndMaxPath)]
     print INTx, INTy
-    return(INTx,INTy)\
+    return(INTx,INTy)
